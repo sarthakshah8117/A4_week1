828a47dc0b28b4292b7c8ba82688ab638897d4bd
Sarthak Shah <sharthakshah81173@gmail.com>
2021-01-19 14:34:28 -0500

    added express

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const http = require('http'); //like a PHP require
-//require is same as JS import
+const express = require('express'); //like a PHP require
+const path = require('path');
+const server = express();
 
-// const hostname = '127.0.0.1';//this is localhost
+
+server.set("views", path.join(__dirname, 'views'));
+
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.get("/", (req, res) => {
+  console.log('hit the home route');
+  res.sendFile('index.html')
+})
+
+server.get("/contact", (req, res) =>{
+  console.log('hit the home route');
+  res.sendFile('contact.html');
+})
+
 const port = process.env.PORT || 3000; //localhost:3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('8 days and World');
-});
 
 server.listen(port, () => {
-  console.log(`Server is now running at http://${hostname}:${port}/`);
+  console.log(`Server is now running at ${port}/`);
 });
